f6acba4162a98a8f78884bf5f78f8a47218d4455
abolo <ali.bolourian@softchoice.com>
2011-06-14 10:29:41 -0400

    Created project

---
+++ Tests/CustomCommands.cs
@@ -0,0 +1,77 @@
+﻿namespace Tests
+{
+    using System;
+    using System.Runtime.Serialization;
+    using System.Windows.Media;
+    using System.Xml;
+
+    namespace TestWithWhite
+    {
+        public interface ITextBoxCommands
+        {
+            Background Background { get; }
+            //        Validation Validation { get; }
+        }
+
+        public class TextBoxCommands : ITextBoxCommands
+        {
+            private readonly System.Windows.Controls.TextBox _textBox;
+
+            public TextBoxCommands(System.Windows.Controls.TextBox textBox)
+            {
+                _textBox = textBox;
+            }
+
+            public Background Background
+            {
+                get { return new Background(_textBox.Background); }
+            }
+
+            public Validation Validation
+            {
+                get { return new Validation(_textBox); }
+            }
+        }
+
+        [DataContract]
+        public class Validation
+        {
+            [DataMember]
+            private readonly bool _hasError;
+
+            public Validation(System.Windows.Controls.TextBox textBox)
+            {
+                _hasError = System.Windows.Controls.Validation.GetHasError(textBox);
+            }
+
+            public bool HasError
+            {
+                get { return _hasError; }
+            }
+        }
+
+        [DataContract]
+        public class Background
+        {
+            [DataMember]
+            private readonly string _color;
+
+            public Background(Brush brush)
+            {
+                if (brush is SolidColorBrush)
+                {
+                    _color = (brush as SolidColorBrush).Color.ToString();
+                }
+                else
+                {
+                    _color = "Bad brush!";
+                }
+            }
+
+            public string Color
+            {
+                get { return _color; }
+            }
+        }
+    }
+}
---
+++ Tests/MyTextBoxTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows.Media;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Tests.TestWithWhite;
+using White.Core;
+using White.Core.CustomCommands;
+using White.Core.UIItems.WindowItems;
+using White.Core.UIItems;
+
+namespace Tests
+{
+    [TestFixture]
+    public class MyTextBoxTests
+    {
+        private Application _application;
+        private Window _horizonWindow;
+
+        [SetUp]
+        public void Setup()
+        {
+            _application = LaunchApplication();
+            _horizonWindow = FindMainWindow();
+
+            CustomCommandSerializer.AddKnownTypes(typeof(Background));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _application.Kill();
+        }
+
+        [Test]
+        public void TestCustomCommands()
+        {
+            var myTextBox = _horizonWindow.Get<TextBox>("myTextBox");
+            var button = _horizonWindow.Get<Button>("changeColorButton");
+            var myCommands = new CustomCommandFactory().Create<ITextBoxCommands>(myTextBox);
+
+            myTextBox.Text = "Hello";
+
+            Assert.That(myCommands.Background.Color, Is.EqualTo(Colors.White.ToString()));
+            Assert.That(myTextBox.Text, Is.EqualTo("Hello"));
+
+            button.Click();
+
+            // These two asserts fail
+//            Assert.That(myCommands.Background.Color, Is.EqualTo(Colors.LightBlue.ToString()));
+//            Assert.That(myTextBox.Text, Is.EqualTo("Hello"));
+
+            // These two asserts pass
+            Assert.That(myTextBox.Text, Is.EqualTo("Hello"));
+            Assert.That(myCommands.Background.Color, Is.EqualTo(Colors.LightBlue.ToString()));
+        }
+
+        private Window FindMainWindow()
+        {
+            return _application.GetWindow("MainWindow");
+        }
+
+        private Application LaunchApplication()
+        {
+            return Application.Launch(@"C:\Projects\Sandbox\WhiteCustomCommands\TestApp\bin\Debug\TestApp.exe");
+        }
+
+    }
+}
